aa1bc3c57d00c098dd38a011f3abff7167da8b29
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2023-08-21 16:34:45 +0700

    progress 01

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    const navbarHeight = navbar.clientHeight;
+    const offset = window.pageYOffset;
+
+    if (offset > navbarHeight) {
+        navbar.classList.add("sticky", "scrolled");
+    } else {
+        navbar.classList.remove("sticky", "scrolled");
+    }
+
+    if (navbarCollapse.classList.contains("active")) {
+        navbarCollapse.style.height = "100%";
+    }
+});
+
+// Navbar Mobile
+const navbarToggler = document.querySelector(".navbar-toggler");
+
+navbarToggler.addEventListener("click", function () {
+    document.querySelector(".navbar-collapse").classList.toggle("active");
+});
